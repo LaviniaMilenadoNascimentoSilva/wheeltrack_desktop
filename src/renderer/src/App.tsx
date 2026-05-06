5d626a9466b9008a20cdf654035d1b8e1886499b
unknown <Lavinia_25968@ETEC.FURLAN>
2026-05-05 21:55:42 -0300

    começando desktop

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import "./app.css"
+import imagem from "./assets/download.jpg"
+
+export default function AQUISÓTEMCABAÇO() {
+  return (
+    <div className="titulo" >
+      <h1>AQUI SÓ TEM CABAÇO</h1>
+        <img src={imagem} alt="" />
+      <p>asdfghjkijuygvbn</p>
+    </div>
+  )
+}
