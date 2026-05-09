e403e002500ac64c1d7f0226d7567782ebc6a9f7
unknown <Lavinia_25968@ETEC.FURLAN>
2026-05-08 22:23:58 -0300

    Alterações do dia 08052026

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,24 @@
 import "./app.css"
 import imagem from "./assets/download.jpg"
+import Contato from "./contato"
+import { Link, Route, Routes } from "react-router-dom"
 
 export default function AQUISÓTEMCABAÇO() {
   return (
-    <div className="titulo" >
-      <h1>AQUI SÓ TEM CABAÇO</h1>
-        <img src={imagem} alt="" />
-      <p>asdfghjkijuygvbn</p>
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <div className="titulo" >
+          <h1>AQUI SÓ TEM CABAÇO</h1>
+            <img src={imagem} alt="" />
+          <p>asdfghjkijuygvbn</p>
+          <p>Para saber mais, visite nossa.</p>  
+          <Link to="/contato">Vá para contato</Link>
+
+        </div>
+      }/>
+      
+      <Route path="/contato" element={<Contato/>} />
+    </Routes>
+    
   )
 }
---
+++ src/renderer/src/contato.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Contato () {
+    return(
+        <h1>VOCÊ ESTÁ EM CONTATO </h1>
+    )
+    
+}
+
+
