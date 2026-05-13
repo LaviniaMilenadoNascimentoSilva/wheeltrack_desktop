b06f9456fe87aaaa84d8729216f36546a06a3dfd
Lavinia M N Silva <lavinia.mnsilva@gmail.com>
2026-05-12 21:01:04 -0300

    Testando links fora do APP

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,23 +2,26 @@ import "./app.css"
 import imagem from "./assets/download.jpg"
 import Contato from "./contato"
 import { Link, Route, Routes } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 export default function AQUISÓTEMCABAÇO() {
   return (
-    <Routes>
-      <Route path="/" element={
-        <div className="titulo" >
-          <h1>AQUI SÓ TEM CABAÇO</h1>
+   
+      <Routes>
+        <Route path="/" element={
+          <div className="titulo" >
+            <h1>AQUI SÓ TEM CABAÇO</h1>
             <img src={imagem} alt="" />
-          <p>asdfghjkijuygvbn</p>
-          <p>Para saber mais, visite nossa.</p>  
-          <Link to="/contato">Vá para contato</Link>
+            <p>asdfghjkijuygvbn</p>
+            <p>Para saber mais, visite nossa.</p>
+            <Link to="/contato">Vá para contato</Link>
 
-        </div>
-      }/>
-      
-      <Route path="/contato" element={<Contato/>} />
-    </Routes>
+          </div>
+        } />
+
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
     
+
   )
 }
--- src/renderer/src/contato.tsx
+++ src/renderer/src/contato.tsx
@@ -1,10 +1,18 @@
+import "./contato.css"
+import imagem from "./assets/wheeltrack02.png"
+import App from "./App";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
+
+export default function Contato() {
+    return (
+        <div className="titulo">
+            <h1>CONTATO</h1>
+            <img src={imagem} alt="Wheeltrack" />
+            <Link to="/" >Voltar</Link>
+        </div>
 
 
-export default function Contato () {
-    return(
-        <h1>VOCÊ ESTÁ EM CONTATO </h1>
     )
-    
 }
 
 
